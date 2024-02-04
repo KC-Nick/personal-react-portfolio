c2c659fdc1e95d8b156bb9ba0f61f31b44031a24
KC-Nick <wilson.kasinichole@gmail.com>
2024-02-03 22:40:35 -0500

    added to about, wrote starter code for other component files

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Hello, and welcome to my portfolio. My name is Nick, and throughout these pages, you will find my resume,
+          works I've done, and ways to contact me. I hope you like what you see, and I look forward to hearing from you,
+          if you have a project you would like me to work on. I have extensive work history in different fields (including,
+          but not limited to; customer service, food service, taking calls and redirecting them, and home healthcare), and I
+          have worked with a wide variety of different people with unique backgrounds. I work well both independently and
+          with a team. I love facing challenges like puzzles that I can crack and improve my way of thinking to something
+          beyond what it was before. I would even go so far as to believe the purpose of life is to learn.
+
+          I dream of being a writer, and video game developer someday, but for now, I am happy with gaining new skills from
+          going new places, and continuing to meet new people. I am open-minded to both others, and problems before me.
+          I am currently attending a bootcamp with Ohio State University, that will grant me a certificate in Full-Stack Web
+          Programming when it is completed. This class taught me multiple tools in a short time that allow me to create complex
+          web applications, and deploy them, as well as approach bugs and problems in effective and creative ways. I hope
+          that you will take a look over my projects and resume, both by myself and with groups, and consider me for your next
+          project.
+        </p>
+      </div>
+    );
+  }
---
+++ src/components/pages/Contact.jsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contacts and Web-Pages</h1>
+        <p>
+          Email; wilson.kasinichole@gmail.com
+          GitHub; https://github.com/KC-Nick/
+          LinkedIn; 
+        </p>
+      </div>
+    );
+  }
